6e433fcc5f3c977a7798935dc0ce9efd94d7cf0c
johntosberg <johntosberg@gmail.com>
2018-01-11 20:41:18 -0600

    Missed a couple files for my living and breathing app

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,9 +1,14 @@
+import domain.Track
+import org.grails.datastore.gorm.neo4j.Neo4jDatastore
 import ratpack.groovy.template.MarkupTemplateModule
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
+
+  Neo4jDatastore datastore = new Neo4jDatastore(['grails.neo4j.url':'bolt://localhost:7687'] as Map, Track)
+
   bindings {
     module MarkupTemplateModule
   }

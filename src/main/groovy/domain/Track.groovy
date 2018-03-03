752a942f93b7e1f67abe3fddb6c1aa03b93e92f8
johnosberg <johntosberg@gmail.com>
2018-03-03 15:11:36 -0600

    Update ignore and get rid of ratpack. Not making a webapp anytime soon and right now it's just clutter

--- src/main/groovy/domain/Track.groovy
+++ src/main/groovy/domain/Track.groovy
@@ -1,10 +1,30 @@
 package domain
 
-import grails.persistence.*
+import org.neo4j.ogm.annotation.GeneratedValue
+import org.neo4j.ogm.annotation.Id
+import org.neo4j.ogm.annotation.NodeEntity
+import org.neo4j.ogm.annotation.Property
+import org.neo4j.ogm.annotation.Relationship
 
-@Entity
-class Track  {
+@NodeEntity
+class Track {
 
+    @Id @GeneratedValue
+    private Long id;
+
+    @Property(name = 'name')
     String name
+
+    @Property(name = 'artist')
+    Artist artist
+
+    @Property(name = 'popularity')
+    Integer popularity
+
+    @Property(name = 'bpm')
+    Integer bpm
+
+    @Relationship(type = 'NEXT_SONG_PLAYED', direction = Relationship.OUTGOING)
+    List<Track> nextSongPlayed
 
 }
---
+++ src/test/groovy/domain/TrackSpec.groovy
@@ -0,0 +1,31 @@
+package domain
+
+import spock.lang.Specification
+
+class TrackSpec extends Specification {
+
+    void 'I can make a track and add the next song played'() {
+        given:
+        Track getLow = new Track(
+                name: 'Get low',
+                artist: new Artist(
+                    name: 'Lil Jon and the East Side Boyz'
+                ),
+                popularity: 89,
+                bpm: 100
+        )
+        Track yeah = new Track(
+                name: 'Yeah!',
+                artist: new Artist(
+                        name: 'Usher'
+                ),
+                popularity: 92,
+                bpm: 110,
+                nextSongPlayed: [getLow]
+        )
+
+        expect:
+        yeah.nextSongPlayed.contains(getLow)
+    }
+
+}
